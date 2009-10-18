4b713343cfeddedbdea506a533fa3a2097c0c552
Hiram Chirino <hiram@hiramchirino.com>
2009-10-18 11:55:30 -0400

    adding in a profile to do interation tests.

--- src/it/resty-gwt-example/src/main/java/com/hiramchirino/restygwt/examples/server/PizzaServlet.java
+++ src/it/resty-gwt-example/src/main/java/com/hiramchirino/restygwt/examples/server/PizzaServlet.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2009  Hiram Chirino
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hiramchirino.restygwt.examples.server;
 
 import java.io.IOException;
@@ -14,6 +29,10 @@ import com.hiramchirino.restygwt.client.Resource;
 import com.hiramchirino.restygwt.examples.client.OrderConfirmation;
 import com.hiramchirino.restygwt.examples.client.PizzaOrder;
 
+/**
+ * 
+ * @author <a href="http://hiramchirino.com">Hiram Chirino</a>
+ */
 public class PizzaServlet extends HttpServlet {
 
     private static final long serialVersionUID = -5364009274470240593L;
