2707aa3239f6fac468684103e95cd1d45a47a83c
Hiram Chirino <hiram@hiramchirino.com>
2010-01-26 11:48:44 -0500

    Adding an integration test/example of how to use jersey with resty-gwt

---
+++ src/it/resty-gwt-jersey-jaxb-example/src/main/java/com/hiramchirino/restygwt/examples/server/JsonJaxbProvider.java
@@ -0,0 +1,30 @@
+package com.hiramchirino.restygwt.examples.server;
+
+import javax.ws.rs.ext.Provider;
+import javax.xml.bind.JAXBContext;
+
+import com.hiramchirino.restygwt.examples.client.MapResult;
+import com.sun.jersey.api.json.JSONConfiguration;
+import com.sun.jersey.api.json.JSONJAXBContext;
+
+@Provider
+public class JsonJaxbProvider {
+    
+    private JAXBContext context;
+    private Class<?>[] types = { MapResult.class };
+
+    public JsonJaxbProvider() throws Exception {
+        JSONConfiguration config = JSONConfiguration.natural().build();
+        context = new JSONJAXBContext(config, types);
+    } 
+
+    public JAXBContext getContext(Class<?> objectType) {
+        for (Class<?> type : types) {
+            if (type == objectType) {
+                return context;
+            }
+        }
+        return null;
+    }
+
+}
