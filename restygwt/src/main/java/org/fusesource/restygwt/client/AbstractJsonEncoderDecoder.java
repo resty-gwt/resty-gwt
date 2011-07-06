bd16ee2997b3a4eb2603a82f9463cefd6291c66d
Kristian Meier <m.kristian@web.de>
2011-07-06 22:11:44 +0530

    use date format string for encoding if set

--- restygwt/src/main/java/org/fusesource/restygwt/client/AbstractJsonEncoderDecoder.java
+++ restygwt/src/main/java/org/fusesource/restygwt/client/AbstractJsonEncoderDecoder.java
@@ -285,7 +285,7 @@ abstract public class AbstractJsonEncoderDecoder<T> implements JsonEncoderDecode
             if (format == null) {
                 return new JSONNumber(value.getTime());
             } else {
-                return new JSONString(DateTimeFormat.getMediumDateTimeFormat().format(value));
+                return new JSONString(DateTimeFormat.getFormat(format).format(value));
             }
         }
     };
