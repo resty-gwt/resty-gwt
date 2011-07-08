42bd9a617ae4807ad141ecc0f83d5da678db7731
Kristian Meier <m.kristian@web.de>
2011-07-08 17:45:30 +0530

    added some example how to use the factory for creating singletons usable as dispatcher annotion options on the restservice interface

--- restygwt/src/main/java/org/fusesource/restygwt/example/client/dispatcher/DispatcherFactory.java
+++ restygwt/src/main/java/org/fusesource/restygwt/example/client/dispatcher/DispatcherFactory.java
@@ -1,9 +1,8 @@
 package org.fusesource.restygwt.example.client.dispatcher;
 
-import org.fusesource.restygwt.client.Dispatcher;
-import org.fusesource.restygwt.client.cache.VolatileQueueableCacheStorage;
 import org.fusesource.restygwt.client.cache.DefaultQueueableCacheStorage;
 import org.fusesource.restygwt.client.cache.QueueableCacheStorage;
+import org.fusesource.restygwt.client.cache.VolatileQueueableCacheStorage;
 import org.fusesource.restygwt.client.callback.CachingCallbackFilter;
 import org.fusesource.restygwt.client.callback.CallbackFactory;
 import org.fusesource.restygwt.client.callback.CallbackFilter;
@@ -16,12 +15,35 @@ import org.fusesource.restygwt.client.dispatcher.CachingDispatcherFilter;
 import org.fusesource.restygwt.client.dispatcher.DefaultDispatcherFilter;
 import org.fusesource.restygwt.client.dispatcher.DefaultFilterawareDispatcher;
 import org.fusesource.restygwt.client.dispatcher.DispatcherFilter;
+import org.fusesource.restygwt.client.dispatcher.FilterawareDispatcher;
 import org.fusesource.restygwt.client.dispatcher.RestfulCachingDispatcherFilter;
 import org.fusesource.restygwt.client.dispatcher.XSSTokenDispatcherFilter;
 
+/**
+ * this factory can be used to create a Dispatcher which can be used as dispatcher Option on
+ * the RestService interface.<br />
+ * <code><pre>
+ * public class RestfulRetryingDispatcherSingleton implements Dispatcher{
+ *
+ *   public static Dispatcher INSTANCE = new DispatcherFactory().restfulCachingDispatcher();
+ *   
+ *   // do not allow concrete instances of this class
+ *   private RestfulRetryingDispatcherSingleton(){
+ *     throw new Error("never called");
+ *   }
+ *
+ *   public Request send(Method method, RequestBuilder builder) throws RequestException {
+ *     return null;   // dummy
+ *   }
+ * }
+ * </pre></code>
+ * 
+ * @author kristian
+ *
+ */
 public class DispatcherFactory {
 
-    Dispatcher xssProtectionDispatcher(){
+    public FilterawareDispatcher xssProtectionDispatcher(){
         XSSToken xss = new XSSToken();
 
         DispatcherFilter xssDispatcherFilter = new XSSTokenDispatcherFilter(xss);
@@ -30,48 +52,48 @@ public class DispatcherFactory {
         CallbackFactory callbackFactory = new DefaultCallbackFactory(xssCallbackFilter);
         DispatcherFilter defaultDispatcherFilter = new DefaultDispatcherFilter(callbackFactory);
         
-        Dispatcher dispatcher = new DefaultFilterawareDispatcher(xssDispatcherFilter, defaultDispatcherFilter);
+        FilterawareDispatcher dispatcher = new DefaultFilterawareDispatcher(xssDispatcherFilter, defaultDispatcherFilter);
         
         return dispatcher;
     }
     
-    Dispatcher cachingDispatcher(){
+    public FilterawareDispatcher cachingDispatcher(){
         QueueableCacheStorage cache = new DefaultQueueableCacheStorage();
         
         CallbackFilter cachingCallbackFilter = new CachingCallbackFilter(cache);
         CallbackFactory callbackFactory = new DefaultCallbackFactory(cachingCallbackFilter);
         DispatcherFilter cachingDispatcherFilter = new CachingDispatcherFilter(cache,callbackFactory);
         
-        Dispatcher dispatcher = new DefaultFilterawareDispatcher(cachingDispatcherFilter);
+        FilterawareDispatcher dispatcher = new DefaultFilterawareDispatcher(cachingDispatcherFilter);
         
         return dispatcher;
     }
     
-    Dispatcher retryingDispatcher(){
+    public FilterawareDispatcher retryingDispatcher(){
         QueueableCacheStorage cache = new VolatileQueueableCacheStorage();
         
         CallbackFilter cachingCallbackFilter = new CachingCallbackFilter(cache);
         CallbackFactory callbackFactory = new RetryingCallbackFactory(cachingCallbackFilter);
         DispatcherFilter cachingDispatcherFilter = new CachingDispatcherFilter(cache, callbackFactory);
         
-        Dispatcher dispatcher = new DefaultFilterawareDispatcher(cachingDispatcherFilter);
+        FilterawareDispatcher dispatcher = new DefaultFilterawareDispatcher(cachingDispatcherFilter);
         
         return dispatcher;
     }
     
-    Dispatcher retryingCachingDispatcher(){
+    public FilterawareDispatcher retryingCachingDispatcher(){
         QueueableCacheStorage cache = new DefaultQueueableCacheStorage();
         
         CallbackFilter cachingCallbackFilter = new CachingCallbackFilter(cache);
         CallbackFactory callbackFactory = new RetryingCallbackFactory(cachingCallbackFilter);
         DispatcherFilter cachingDispatcherFilter = new CachingDispatcherFilter(cache, callbackFactory);
         
-        Dispatcher dispatcher = new DefaultFilterawareDispatcher(cachingDispatcherFilter);
+        FilterawareDispatcher dispatcher = new DefaultFilterawareDispatcher(cachingDispatcherFilter);
         
         return dispatcher;
     }
 
-    Dispatcher retryingCachingXSSProtectionDispatcher(){
+    public FilterawareDispatcher retryingCachingXSSProtectionDispatcher(){
         XSSToken xss = new XSSToken();
 
         DispatcherFilter xssDispatcherFilter = new XSSTokenDispatcherFilter(xss);
@@ -84,36 +106,36 @@ public class DispatcherFactory {
         CallbackFactory callbackFactory = new RetryingCallbackFactory(xssCallbackFilter, cachingCallbackFilter);
         DispatcherFilter cachingDispatcherFilter = new CachingDispatcherFilter(cache, callbackFactory);
         
-        Dispatcher dispatcher = new DefaultFilterawareDispatcher(xssDispatcherFilter, cachingDispatcherFilter);
+        FilterawareDispatcher dispatcher = new DefaultFilterawareDispatcher(xssDispatcherFilter, cachingDispatcherFilter);
         
         return dispatcher;
     }
     
-    Dispatcher restfulCachingDispatcher(){
+    public FilterawareDispatcher restfulCachingDispatcher(){
         QueueableCacheStorage cache = new DefaultQueueableCacheStorage();
         
         CallbackFilter cachingCallbackFilter = new RestfulCachingCallbackFilter(cache);
         CallbackFactory callbackFactory = new DefaultCallbackFactory(cachingCallbackFilter);
         DispatcherFilter cachingDispatcherFilter = new RestfulCachingDispatcherFilter(cache, callbackFactory);
         
-        Dispatcher dispatcher = new DefaultFilterawareDispatcher(cachingDispatcherFilter);
+        FilterawareDispatcher dispatcher = new DefaultFilterawareDispatcher(cachingDispatcherFilter);
         
         return dispatcher;
     }
 
-    Dispatcher restfulRetryingCachingDispatcher(){
+    public FilterawareDispatcher restfulRetryingCachingDispatcher(){
         QueueableCacheStorage cache = new DefaultQueueableCacheStorage();
         
         CallbackFilter cachingCallbackFilter = new RestfulCachingCallbackFilter(cache);
         CallbackFactory callbackFactory = new RetryingCallbackFactory(cachingCallbackFilter);
         DispatcherFilter cachingDispatcherFilter = new RestfulCachingDispatcherFilter(cache, callbackFactory);
         
-        Dispatcher dispatcher = new DefaultFilterawareDispatcher(cachingDispatcherFilter);
+        FilterawareDispatcher dispatcher = new DefaultFilterawareDispatcher(cachingDispatcherFilter);
         
         return dispatcher;
     }
     
-    Dispatcher restfulRetryingCachingXSSProtectionDispatcher(){
+    public FilterawareDispatcher restfulRetryingCachingXSSProtectionDispatcher(){
         XSSToken xss = new XSSToken();
 
         DispatcherFilter xssDispatcherFilter = new XSSTokenDispatcherFilter(xss);
@@ -126,7 +148,7 @@ public class DispatcherFactory {
         CallbackFactory callbackFactory = new RetryingCallbackFactory(xssCallbackFilter, cachingCallbackFilter);
         DispatcherFilter cachingDispatcherFilter = new RestfulCachingDispatcherFilter(cache, callbackFactory);
         
-        Dispatcher dispatcher = new DefaultFilterawareDispatcher(xssDispatcherFilter, cachingDispatcherFilter);
+        FilterawareDispatcher dispatcher = new DefaultFilterawareDispatcher(xssDispatcherFilter, cachingDispatcherFilter);
         
         return dispatcher;
     }
