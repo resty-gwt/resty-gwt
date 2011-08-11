091d87c06c7507f8d742130c90d139c63d7d33a5
Tim Eggert <tim@elbart.com>
2011-08-11 17:17:43 +0200

    removed unused method "_onResponseReceived"

--- restygwt/src/main/java/org/fusesource/restygwt/client/callback/FilterawareRetryingCallback.java
+++ restygwt/src/main/java/org/fusesource/restygwt/client/callback/FilterawareRetryingCallback.java
@@ -2,13 +2,13 @@
  * Copyright (C) 2009-2010 the original author or authors.
  * See the notice.md file distributed with this work for additional
  * information regarding copyright ownership.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -92,19 +92,6 @@ public class FilterawareRetryingCallback implements FilterawareRequestCallback {
         }
 
         requestCallback.onResponseReceived(request, response);
-    }
-
-    /**
-     * replacement for the override of {@link #onResponseReceived(Request, Response)}
-     * 
-     * but this is set to final because of the filter handling, this method has to be
-     * implemented instead.
-     * 
-     * @param request
-     * @param response
-     */
-    protected void _onResponseReceived(Request request, Response response) {
-
     }
 
     /**
