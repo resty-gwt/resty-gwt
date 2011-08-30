aced5b730ebd656930ef1f081328eae0088979c1
sloosaj <tony@architecit.nl>
2011-08-30 21:46:12 +0200

    In order to support POJO's with final fields, the JsonCreator tag should be supported. I created an implementation within the JsonEncoderDecoderClassCreator class. It supports the following construct:
    @JsonTypeName("Credentials")
    public class Credentials {
        @JsonProperty
        private final String password;
        @JsonProperty
        private final String email;
    
        @JsonCreator
        public Credentials(@JsonProperty("email") String email, @JsonProperty("password") String password) {
            this.email = email;
            this.password = password;
        }
    
        public String getEmail() {
            return email;
        }
    
        public String getPassword() {
            return password;
        }
    }
    I added the following helper methods:
    
    private List getOrderedFields(List fields, JConstructor creator) throws UnableToCompleteException;
    private JConstructor findCreator(JClassType sourceClazz);
    
    And the following Jackson annotations:
    
    @JsonCreator
    @JsonProperty

---
+++ restygwt/src/main/java/org/codehaus/jackson/annotate/JsonCreator.java
@@ -0,0 +1,8 @@
+package org.codehaus.jackson.annotate;
+
+@java.lang.annotation.Target(value={java.lang.annotation.ElementType.METHOD,java.lang.annotation.ElementType.CONSTRUCTOR})
+@java.lang.annotation.Retention(value=java.lang.annotation.RetentionPolicy.RUNTIME)
+@org.codehaus.jackson.annotate.JacksonAnnotation
+public @interface JsonCreator {
+
+}
