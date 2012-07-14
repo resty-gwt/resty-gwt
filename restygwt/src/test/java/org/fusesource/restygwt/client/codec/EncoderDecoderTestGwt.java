efeea3401a6c92e0f9d26fb902ffb2881fdc74d5
Kristian Meier <m.kristian@web.de>
2012-07-14 23:17:13 +0530

    added test for arrays de/encoding

--- restygwt/src/test/java/org/fusesource/restygwt/client/codec/EncoderDecoderTestGwt.java
+++ restygwt/src/test/java/org/fusesource/restygwt/client/codec/EncoderDecoderTestGwt.java
@@ -20,10 +20,12 @@ package org.fusesource.restygwt.client.codec;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.codehaus.jackson.annotate.JsonIgnore;
 import org.fusesource.restygwt.client.AbstractJsonEncoderDecoder;
 import org.fusesource.restygwt.client.JsonEncoderDecoder;
 import org.fusesource.restygwt.client.ObjectEncoderDecoder;
@@ -303,5 +305,76 @@ public class EncoderDecoderTestGwt extends GWTTestCase {
         assertEquals(b.toString(),
                 AbstractJsonEncoderDecoder.STRING.decode(AbstractJsonEncoderDecoder.BOOLEAN.encode(b)));
     }
+    
+    public void testBooleanArrayDecode() {
+        boolean[] array = {true, false};
+        AbstractJsonEncoderDecoder<Boolean> encoder = AbstractJsonEncoderDecoder.BOOLEAN;
+        assertEquals(Arrays.toString(array),
+                Arrays.toString(AbstractJsonEncoderDecoder.toArray(AbstractJsonEncoderDecoder.toJSON(array, encoder), 
+                            encoder, new boolean[2])));
+    }
 
+    public void testByteArrayDecode() {
+        byte[] array = {2, 8};
+        AbstractJsonEncoderDecoder<Byte> encoder = AbstractJsonEncoderDecoder.BYTE;
+        assertEquals(Arrays.toString(array),
+                Arrays.toString(AbstractJsonEncoderDecoder.toArray(AbstractJsonEncoderDecoder.toJSON(array, encoder), 
+                            encoder, new byte[2])));
+    }
+
+    public void testCharacterArrayDecode() {
+        char[] array = {'a','z'};
+        AbstractJsonEncoderDecoder<Character> encoder = AbstractJsonEncoderDecoder.CHAR;
+        assertEquals(Arrays.toString(array),
+                Arrays.toString(AbstractJsonEncoderDecoder.toArray(AbstractJsonEncoderDecoder.toJSON(array, encoder), 
+                            encoder, new char[2])));
+    }
+
+    public void testFloatArrayDecode() {
+        float[] array = {1.4e19f, -13.53e-18f};
+        AbstractJsonEncoderDecoder<Float> encoder = AbstractJsonEncoderDecoder.FLOAT;
+        assertEquals(Arrays.toString(array),
+                Arrays.toString(AbstractJsonEncoderDecoder.toArray(AbstractJsonEncoderDecoder.toJSON(array, encoder), 
+                            encoder, new float[2])));
+    }
+
+    public void testDoubleArrayDecode() {
+        double[] array = {1.4e193, -13.53e-188};
+        AbstractJsonEncoderDecoder<Double> encoder = AbstractJsonEncoderDecoder.DOUBLE;
+        assertEquals(Arrays.toString(array),
+                Arrays.toString(AbstractJsonEncoderDecoder.toArray(AbstractJsonEncoderDecoder.toJSON(array, encoder), 
+                            encoder, new double[2])));
+    }
+
+    public void testShortArrayDecode() {
+        short[] array = {1, -13};
+        AbstractJsonEncoderDecoder<Short> encoder = AbstractJsonEncoderDecoder.SHORT;
+        assertEquals(Arrays.toString(array),
+                Arrays.toString(AbstractJsonEncoderDecoder.toArray(AbstractJsonEncoderDecoder.toJSON(array, encoder), 
+                            encoder, new short[2])));
+    }
+
+    public void testIntArrayDecode() {
+        int[] array = {1010, -13100};
+        AbstractJsonEncoderDecoder<Integer> encoder = AbstractJsonEncoderDecoder.INT;
+        assertEquals(Arrays.toString(array),
+                Arrays.toString(AbstractJsonEncoderDecoder.toArray(AbstractJsonEncoderDecoder.toJSON(array, encoder), 
+                            encoder, new int[2])));
+    }
+
+    public void testLongArrayDecode() {
+        long[] array = {1010, -13100};
+        AbstractJsonEncoderDecoder<Long> encoder = AbstractJsonEncoderDecoder.LONG;
+        assertEquals(Arrays.toString(array),
+                Arrays.toString(AbstractJsonEncoderDecoder.toArray(AbstractJsonEncoderDecoder.toJSON(array, encoder), 
+                            encoder, new long[2])));
+    }
+
+    public void testTypeArrayDecode() {
+        String[] array = {"may", "all", "be", "happy"};
+        AbstractJsonEncoderDecoder<String> encoder = AbstractJsonEncoderDecoder.STRING;
+        assertEquals(Arrays.toString(array),
+                Arrays.toString(AbstractJsonEncoderDecoder.toArray(AbstractJsonEncoderDecoder.toJSON(array, encoder), 
+                            encoder, new String[4])));
+    }
 }
