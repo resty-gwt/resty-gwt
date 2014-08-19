ee690b130dd608f2255637662737375f442a3178
Vassilis Virvilis <vasvir2@gmail.com>
2014-08-19 17:35:15 +0300

    List, Set, and OrderedSet in form should be handled as per jax-rs standard Look at page 12 In section 3.2 of the spec. This makes it work with CXF given that you have given CXF a jackson ParamConverter

--- restygwt/src/main/java/org/fusesource/restygwt/client/FormPostContent.java
+++ restygwt/src/main/java/org/fusesource/restygwt/client/FormPostContent.java
@@ -3,7 +3,7 @@ package org.fusesource.restygwt.client;
 import com.google.gwt.http.client.URL;
 
 public class FormPostContent {
-    private String textContent = "";
+    private StringBuilder textContent = new StringBuilder();
     private boolean ampersand;
 
     public void addParameter(String name, String value) {
@@ -11,14 +11,32 @@ public class FormPostContent {
             return;
         }
 
-        textContent += (ampersand ? "&" : "") +
+        textContent.append((ampersand ? "&" : "") +
                 URL.encodeQueryString(name) +
                 "=" +
-                URL.encodeQueryString(value);
+                URL.encodeQueryString(value));
         ampersand = true;
     }
 
+    public void addParameters(String key, Iterable<String> values) {
+        if (values == null)
+            return;
+        key = URL.encodeQueryString(key);
+        textContent.append((ampersand ? "&" : ""));
+        for (String value : values) {
+            if (value == null)
+                continue;
+            if (ampersand) {
+                textContent.append('&');
+            } else {
+                ampersand = true;
+            }
+            value = URL.encodeQueryString(value);
+            textContent.append(key + "=" + value);
+        }
+    }
+
     public String getTextContent() {
-        return textContent;
+        return textContent.toString();
     }
 }
