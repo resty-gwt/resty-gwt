5380af847ca34d07e301b08cad5935420f9237dd
Maia Everett <maia@lucidfox.org>
2014-11-01 19:01:19 +0600

    Update Jackson annotations to version 2.4

--- restygwt/src/main/java/com/fasterxml/jackson/annotation/JacksonAnnotationsInside.java
+++ restygwt/src/main/java/com/fasterxml/jackson/annotation/JacksonAnnotationsInside.java
@@ -5,9 +5,21 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ ElementType.ANNOTATION_TYPE })
+/**
+ * Meta-annotation (annotations used on other annotations)
+ * used for indicating that instead of using target annotation
+ * (annotation annotated with this annotation),
+ * Jackson should use meta-annotations it has.
+ * This can be useful in creating "combo-annotations" by having
+ * a container annotation, which needs to be annotated with this
+ * annotation as well as all annotations it 'contains'.
+ * 
+ * @since 2.0
+ */
+@Target({ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @JacksonAnnotation
-public @interface JacksonAnnotationsInside {
+public @interface JacksonAnnotationsInside
+{
 
 }
