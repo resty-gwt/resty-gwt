5b9c194a441a85dafabbfb5135a026f87251f474
Ralf Sommer <ralf.sommer.dev@gmail.com>
2015-11-25 23:32:06 +0100

    Add Test for #249

---
+++ restygwt/src/test/java/org/fusesource/restygwt/client/codec/PolymorphicEncoderDecoderTestGwt.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (C) 2009-2015 the original author or authors.
+ * See the notice.md file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.fusesource.restygwt.client.codec;
+
+import org.fusesource.restygwt.client.JsonEncoderDecoder;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class PolymorphicEncoderDecoderTestGwt extends GWTTestCase {
+
+    @Override
+    public String getModuleName() {
+        return "org.fusesource.restygwt.EncoderDecoderTestGwt";
+    }
+
+    @JsonSubTypes({ @Type(A.class), @Type(B.class) })
+    @JsonTypeName("A")
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@class")
+    static public class A {
+        private String code;
+
+        public String getCode() {
+            return code;
+        }
+
+        public void setCode(String code) {
+            this.code = code;
+        }
+    }
+
+    @JsonTypeName("B")
+    static public class B extends A {
+        private String desc;
+
+        public String getDesc() {
+            return desc;
+        }
+
+        public void setDesc(String desc) {
+            this.desc = desc;
+        }
+    }
+
+    static public interface IdNamePolymorphicCodec extends JsonEncoderDecoder<A> {
+    }
+
+    public void testIdNamePolymorphic() {
+        IdNamePolymorphicCodec codec = GWT.create(IdNamePolymorphicCodec.class);
+
+        String json = "{\"code\":\"test code\"}";
+
+        A obj = codec.decode(json);
+    }
+
+}
