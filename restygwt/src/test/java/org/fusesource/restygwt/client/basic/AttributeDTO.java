1f54744838222283d41b8b687d9ae2469e6746f7
Thomas Cybulski <tcybulsk@de.hellmann.net>
2016-01-22 10:44:13 +0100

    Added the feature in RestServiceClassCreator to work with public getter-Methods and public fields in case of using attribute annotations.

---
+++ restygwt/src/test/java/org/fusesource/restygwt/client/basic/AttributeDTO.java
@@ -0,0 +1,24 @@
+package org.fusesource.restygwt.client.basic;
+
+public class AttributeDTO {
+
+	public Long publicId;
+	private Long privateId;
+	private String path;
+
+	public Long getPrivateId() {
+		return privateId;
+	}
+
+	public void setPrivateId(Long privateId) {
+		this.privateId = privateId;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+}
