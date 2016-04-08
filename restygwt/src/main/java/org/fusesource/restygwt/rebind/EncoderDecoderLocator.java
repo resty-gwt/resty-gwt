b7e320ac9833e6f8aa912b6142fd15b3c1314e8b
lucasam <lucasam@gmail.com>
2016-04-08 16:13:22 -0300

    Added GWT-Jackson dependency and EncoderDecoders. Unit tests not present at this commit

---
+++ restygwt/src/main/java/org/fusesource/restygwt/rebind/EncoderDecoderLocator.java
@@ -0,0 +1,22 @@
+package org.fusesource.restygwt.rebind;
+
+import org.fusesource.restygwt.client.Json.Style;
+
+import com.google.gwt.core.ext.UnableToCompleteException;
+import com.google.gwt.core.ext.typeinfo.JClassType;
+import com.google.gwt.core.ext.typeinfo.JType;
+
+public interface EncoderDecoderLocator {
+
+
+	public String encodeExpression(JType type, String expression, Style style) throws UnableToCompleteException;
+
+	public String decodeExpression(JType type, String expression, Style style) throws UnableToCompleteException;
+
+	//TODO remove this methods
+	public boolean hasCustomEncoderDecoder(JType type);
+
+	public boolean isCollectionType(JClassType clazz) ;
+	
+	public JClassType getListType();
+}
---
+++ restygwt/src/test/java/org/fusesource/restygwt/client/basic/BasicGwtJacksonTestGwt.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (C) 2009-2012 the original author or authors.
+ * See the notice.md file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.fusesource.restygwt.client.basic;
+
+import org.fusesource.restygwt.client.Resource;
+import org.fusesource.restygwt.client.RestServiceProxy;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ *
+ *
+ * @author <a href="mailto:mail@raphaelbauer.com">rEyez</<a>
+ *
+ */
+public class BasicGwtJacksonTestGwt extends GWTTestCase {
+
+    @Override
+    public String getModuleName() {
+        return "org.fusesource.restygwt.BasicGwtJacksonTestGwt";
+    }
+    
+    public void testDefaultFunction() {
+        //configure RESTY
+        Resource resource = new Resource(GWT.getModuleBaseURL() + "api/getendpoint");
+
+        ExampleService service = GWT.create(ExampleService.class);
+        ((RestServiceProxy) service).setResource(resource);
+    }
+
+    
+}
